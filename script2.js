3c84acefcfbdd16971d96c1e614e4a7066ce2543
MangaiyarkarasiM <mangaivmm@gmail.com>
2021-10-07 10:17:13 +0530

    Countries with less than 2 Lakhs population

---
+++ script2.js
@@ -0,0 +1,8 @@
+var req=new XMLHttpRequest();
+req.open('GET','https://restcountries.com/v2/all',true);
+req.send();
+req.onload = () =>{
+    var result=JSON.parse(req.response);
+    var res=result.filter((ele)=>ele.population<200000);
+    console.log(res);
+}
